e8eeb7b1dc47771fbf22b581730478d3121e808f
Bruno-A-Z <zanateli@hotmail.com>
2026-08-07 15:44:07 -0300

    Initial commit: Starting Java CP04

---
+++ src/test/java/com/java/cp04/Cp04ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.java.cp04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cp04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
