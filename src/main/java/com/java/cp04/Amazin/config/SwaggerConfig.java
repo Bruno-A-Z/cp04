1ebbdefe4b777e08226e6618ffd116841c55869e
Bruno-A-Z <zanateli07@outlook.com>
2026-08-17 20:28:36 -0300

    Fix: fixed Swagger name

--- src/main/java/com/java/cp04/Amazin/config/SwaggerConfig.java
+++ src/main/java/com/java/cp04/Amazin/config/SwaggerConfig.java
@@ -12,7 +12,7 @@ public class SwaggerConfig {
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .info(new Info()
-                        .title("Amazin - Mercado Express API")
+                        .title("Amazin - Mercado Express API CP04")
                         .version("1.0")
                         .description("API REST para gestão de itens de um mercado express, com persistência Oracle, Lombok e HATEOAS."));
     }
